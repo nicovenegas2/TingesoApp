afa12054f3640e0349225ccc71553060f980a233
Nicolas Venegas <nicolas.venegas.a@usach.cl>
2022-10-05 16:02:38 -0300

    changes

--- files/DummyEmployee.sql
+++ files/DummyEmployee.sql
@@ -1,4 +1,4 @@
-INSERT INTO `Employees` (`id_employees`,`name`,`id_category`,`last_name`,`entry_date`,`birth`)
+INSERT INTO `Employees` (`id_employees`,`name_employee`,`id_category`,`last_name`,`entry_date`,`birth`)
 VALUES
   ("13.111.192-3","Thor",3,"Benjamin","2023-09-21","2022-06-23"),
   ("93.236.412-8","Steel",3,"Emerson","2021-11-19","2022-02-13"),
@@ -10,7 +10,7 @@ VALUES
   ("38.679.332-8","Arden",2,"Oneil","2022-07-23","2023-05-18"),
   ("34.431.251-4","Brett",3,"Kaufman","2021-03-03","2019-05-09"),
   ("75.238.262-8","Hoyt",2,"Davidson","2021-11-25","2020-11-26");
-INSERT INTO `Employees` (`id_employees`,`name`,`id_category`,`last_name`,`entry_date`,`birth`)
+INSERT INTO `Employees` (`id_employees`,`name_employee`,`id_category`,`last_name`,`entry_date`,`birth`)
 VALUES
   ("92.746.167-2","Willa",1,"Jackson","2021-04-16","2018-09-27"),
   ("27.768.017-2","Evan",2,"Hansen","2023-01-10","2023-07-12"),
@@ -22,7 +22,7 @@ VALUES
   ("42.262.834-1","Nell",3,"Beck","2021-10-20","2022-03-24"),
   ("66.788.327-5","Ainsley",3,"Daniels","2021-05-28","2020-02-21"),
   ("82.381.578-4","Mercedes",2,"Gordon","2022-08-12","2021-02-19");
-INSERT INTO `Employees` (`id_employees`,`name`,`id_category`,`last_name`,`entry_date`,`birth`)
+INSERT INTO `Employees` (`id_employees`,`name_employee`,`id_category`,`last_name`,`entry_date`,`birth`)
 VALUES
   ("58.328.619-6","Teagan",2,"Sosa","2023-08-27","2022-12-23"),
   ("72.373.458-8","Xandra",2,"Monroe","2021-12-06","2021-04-07"),
@@ -34,7 +34,7 @@ VALUES
   ("53.573.108-4","Kerry",2,"Jimenez","2021-12-10","2021-04-24"),
   ("26.402.801-4","Daryl",3,"Schultz","2021-11-15","2023-01-16"),
   ("51.127.021-5","Giselle",2,"Blevins","2021-03-31","2021-02-09");
-INSERT INTO `Employees` (`id_employees`,`name`,`id_category`,`last_name`,`entry_date`,`birth`)
+INSERT INTO `Employees` (`id_employees`,`name_employee`,`id_category`,`last_name`,`entry_date`,`birth`)
 VALUES
   ("25.365.862-8","Alexander",1,"Wilkins","2022-10-09","2023-07-20"),
   ("38.521.345-2","Lysandra",2,"White","2023-04-01","2021-08-13"),
@@ -46,7 +46,7 @@ VALUES
   ("67.558.191-8","Walter",2,"Mayer","2023-03-02","2021-06-13"),
   ("59.277.898-2","Duncan",3,"Justice","2022-11-11","2020-07-27"),
   ("83.141.426-0","Cameran",1,"Vargas","2022-03-22","2023-05-29");
-INSERT INTO `Employees` (`id_employees`,`name`,`id_category`,`last_name`,`entry_date`,`birth`)
+INSERT INTO `Employees` (`id_employees`,`name_employee`,`id_category`,`last_name`,`entry_date`,`birth`)
 VALUES
   ("07.627.221-5","Brandon",2,"Albert","2021-05-08","2022-11-15"),
   ("58.582.815-3","Mara",3,"Talley","2021-05-08","2021-10-31"),
@@ -58,7 +58,7 @@ VALUES
   ("48.189.597-5","Debra",1,"Chaney","2022-08-07","2021-02-05"),
   ("16.177.294-9","Dahlia",3,"Taylor","2021-10-06","2022-08-24"),
   ("21.525.450-7","Quyn",3,"Stevenson","2022-11-30","2022-10-25");
-INSERT INTO `Employees` (`id_employees`,`name`,`id_category`,`last_name`,`entry_date`,`birth`)
+INSERT INTO `Employees` (`id_employees`,`name_employee`,`id_category`,`last_name`,`entry_date`,`birth`)
 VALUES
   ("55.439.125-0","John",2,"Olson","2022-02-24","2019-01-02"),
   ("44.305.153-1","Brooke",1,"Wilcox","2023-07-15","2019-10-19"),
@@ -70,7 +70,7 @@ VALUES
   ("52.367.408-5","Alexander",1,"Flores","2023-05-31","2023-06-10"),
   ("53.972.376-0","Kylie",2,"Hansen","2021-07-08","2021-01-12"),
   ("75.225.752-1","Wyoming",2,"Tran","2021-05-08","2018-09-16");
-INSERT INTO `Employees` (`id_employees`,`name`,`id_category`,`last_name`,`entry_date`,`birth`)
+INSERT INTO `Employees` (`id_employees`,`name_employee`,`id_category`,`last_name`,`entry_date`,`birth`)
 VALUES
   ("37.735.112-7","Alana",1,"Sharpe","2021-03-08","2021-11-27"),
   ("12.048.946-3","Emery",3,"Kinney","2022-09-16","2021-02-08"),
@@ -82,7 +82,7 @@ VALUES
   ("55.676.172-0","Shea",2,"Parks","2023-07-25","2022-09-07"),
   ("79.555.635-7","Lenore",3,"Valentine","2023-09-15","2021-04-27"),
   ("92.640.544-6","Benedict",1,"Morrison","2023-08-22","2022-06-30");
-INSERT INTO `Employees` (`id_employees`,`name`,`id_category`,`last_name`,`entry_date`,`birth`)
+INSERT INTO `Employees` (`id_employees`,`name_employee`,`id_category`,`last_name`,`entry_date`,`birth`)
 VALUES
   ("68.370.971-1","Bruno",2,"Bishop","2022-01-02","2019-04-30"),
   ("77.785.872-3","Jordan",3,"Carter","2023-01-13","2022-02-12"),
@@ -94,7 +94,7 @@ VALUES
   ("35.257.419-8","Baxter",1,"Frank","2023-02-18","2021-12-11"),
   ("61.742.760-5","Ira",1,"Cunningham","2022-06-17","2020-02-05"),
   ("21.772.164-5","Daria",2,"Webb","2022-12-02","2023-01-13");
-INSERT INTO `Employees` (`id_employees`,`name`,`id_category`,`last_name`,`entry_date`,`birth`)
+INSERT INTO `Employees` (`id_employees`,`name_employee`,`id_category`,`last_name`,`entry_date`,`birth`)
 VALUES
   ("20.616.587-9","Wyatt",1,"Jacobson","2022-02-03","2023-02-18"),
   ("24.540.463-0","Blythe",2,"Kramer","2023-03-16","2022-12-07"),
@@ -106,7 +106,7 @@ VALUES
   ("74.597.381-1","Brent",1,"Norris","2021-05-22","2020-06-08"),
   ("67.637.237-5","Scott",3,"White","2022-09-12","2020-01-11"),
   ("56.002.924-2","Hilary",3,"Chavez","2021-06-23","2020-03-20");
-INSERT INTO `Employees` (`id_employees`,`name`,`id_category`,`last_name`,`entry_date`,`birth`)
+INSERT INTO `Employees` (`id_employees`,`name_employee`,`id_category`,`last_name`,`entry_date`,`birth`)
 VALUES
   ("57.352.384-6","Josiah",1,"Rush","2022-03-25","2018-12-25"),
   ("38.684.594-6","Ria",3,"Patrick","2023-04-30","2021-07-31"),
